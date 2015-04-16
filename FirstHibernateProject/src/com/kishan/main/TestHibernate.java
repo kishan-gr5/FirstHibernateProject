95564a5e4227041821c58237b47edbe5cdcfb30c
Kishan G R <kishangr@Est-KishanG.estuate.com>
2015-04-16 17:40:54 +0530

    commit(16/4/15)

---
+++ FirstHibernateProject/src/com/kishan/main/TestHibernate.java
@@ -0,0 +1,49 @@
+package com.kishan.main;
+
+import java.util.Date;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+
+import com.kishan.hibernate.dto.Address;
+import com.kishan.hibernate.dto.UserDetails;
+
+public class TestHibernate 
+{
+	public static void main(String[] args) 
+	{
+		UserDetails user = new UserDetails();
+			user.setUserName("First user");
+		user.setJoinedDate(new Date());
+		
+		Address addr = new Address();
+		addr.setStreet("yelahanka");
+		addr.setCity("bangalore");
+		user.setHomeAddress(addr);
+		
+		Address addr2 = new Address();
+		addr.setStreet("new bel");
+		addr.setCity("bangalore");
+		user.setOfficeAddress(addr2);
+		
+		SessionFactory sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(user);
+		session.getTransaction().commit();
+		System.out.println("success");
+		session.close();
+		
+		user = null;
+		session = sessionFactory.openSession();
+		session.beginTransaction();
+		user =(UserDetails)session.get(UserDetails.class,1);
+		System.out.println("username retrieved is:"+ user.getUserName());
+	
+		System.out.println("username retrieved is:"+ user.getJoinedDate());
+		System.out.println("successfully retrieved");
+		session.close();
+	}
+}
